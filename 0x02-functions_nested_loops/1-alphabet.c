1bdcb2b9c32fefef955434908e85e0bcef7c47b3
slyvia <slyvia.njuguna@gmail.com>
2022-03-15 12:05:04 +0300

    C-Functions, nested loops project commits

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "_putchar.h"
 
 /**
  * Prints the alphabet in lowercase.
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "_putchar.h"
 
 /**
  * Prints the alphabet 10 times.
