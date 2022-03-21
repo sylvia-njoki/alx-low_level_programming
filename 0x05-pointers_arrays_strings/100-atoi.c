b069626add6700c8734549df9e1b1d97ef65be15
slyvia <slyvia.njuguna@gmail.com>
2022-03-21 15:03:59 +0300

    0x05. C - Pointers, arrays and strings project commits.

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer.
+ * @s: input string.
+ * Return: integer.
+ */
+int _atoi(char *s)
+{
+	unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
+	
+
+	return (oi * pn);
+}
